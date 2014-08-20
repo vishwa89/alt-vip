16c7b7395660b513971d3e05f4c423c2c525a561
vishwa89 <vishwanath89@gmail.com>
2014-08-20 12:30:22 +0530

    vip-top and vip-sidebar style changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -15,7 +15,51 @@ $(function(){
     $('#goup').css('bottom','-1500px');
   });
 
+
+  
+
 });
+
+
+$(document).on('mouseover', '#vipSidebar li:nth-child(1) a img', function() {
+
+  $("#vipSidebar li:nth-child(1) .name").show();
+  $("#vipSidebar li:nth-child(1) .role").show();
+     
+});
+
+$(document).on('mouseover', '#vipSidebar li:nth-child(2) a img', function() {
+
+  $("#vipSidebar li:nth-child(2) .name").show();
+  $("#vipSidebar li:nth-child(2) .role").show();
+     
+});
+$(document).on('mouseover', '#vipSidebar li:nth-child(3) a img', function() {
+
+  $("#vipSidebar li:nth-child(3) .name").show();
+  $("#vipSidebar li:nth-child(3) .role").show();
+     
+});
+$(document).on('mouseover', '#vipSidebar li:nth-child(4) a img', function() {
+
+  $("#vipSidebar li:nth-child(4) .name").show();
+  $("#vipSidebar li:nth-child(4) .role").show();
+     
+});
+
+
+
+$(document).on('mouseleave', '#vipSidebar li a img', function() {
+
+  $("#vipSidebar li .name").hide();
+  $("#vipSidebar li .role").hide();
+     
+});
+
+
+
+ 
+
 
 $(document).ready(function(){
   
@@ -32,4 +76,5 @@ $(document).ready(function(){
     marginY: 7,
   });
  });
+
 
